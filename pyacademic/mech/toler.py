ac358fc6508a93d82c5c46c2534876821c1d9be9
m-luk <mlukaszewicz2@gmail.com>
2022-01-30 19:57:34 +0100

    mech.toler updates, argparse
    toler.py:
    - added argparse interface
    - refactoring and documentation update
    - fixed bug in it.csv (duplicate IT)
    
    For: #3

--- pyacademic/mech/toler.py
+++ pyacademic/mech/toler.py
@@ -2,13 +2,13 @@
 IT tolerance calculating script with CLI
 """
 
-import os
-from math import *
+import argparse
+import sys
 from os import system
-from pathlib import Path
 
 import pandas as pd
 from art import *
+
 from . import DATA_DIR
 
 # path to tolerance table
@@ -21,10 +21,18 @@ its.remove("min")
 its.remove("max")
 
 
-def get_toler(dim, it):
-    ''' Returns International Tolerance value for given dimension(dim) and class(it)'''
+def get_tolerance(dim: float, it: int) -> float:
+    """ get IT tolerance value for given dimension in given class
+
+    Args:
+        dim (float): tolerated dimension value in mm
+        it (int): tolerance class
+
+    Returns:
+        float: tolerance value
+    """
     # check if it in data
-    if it not in its:
+    if str(it) not in its:
         print("#### IT not supported ####\n")
         return False
 
@@ -40,42 +48,62 @@ def get_toler(dim, it):
         return False
 
     # get toler value
-    toler = df[it][(df["min"] < dim) & (df["max"] >= dim)]
+    toler = df[str(it)][(df["min"] < dim) & (df["max"] >= dim)]
 
     return round(toler.to_list()[0], 5)
 
 
-def main():
-    # clear console
-    _ = system("cls")
+def get_tolerance_str(dim: float, it: int) -> float:
+    """ run get_tolerance and print the output with info """
+    tolerance = get_tolerance(dim, it)
+    print(
+        f"Tolerance for dim={dim} [mm] in IT{it} is: {tolerance} [mm]")
+    return tolerance
 
+
+def run_ui():
+    """ run interface """
+    # clear console
     print("toler.py 2021 v0.1\nInsert \'q\' followed by <ENTER> to exit, or CTRL+C\n")
     print(text2art("TOLER.PY"))
 
     # program loop
     while True:
         # get dimension from user
-        dim = input("\nInsert dimension: ").strip()
+        user_dim = input("\nInsert dimension: ").strip()
 
-        if dim == 'q':
+        if user_dim == 'q':
             break
 
         # get IT from user
-        it = input("Insert IT: ").strip()
+        user_it = input("Insert IT: ").strip()
 
-        if it == 'q':
+        if user_it == 'q':
             break
 
-        toler = get_toler(dim, it)
-
-        if toler:
-            print("\n Tolerance for dim={} int IT{} is: {} [mm]".format(
-                dim,
-                it,
-                toler
-            ))
-        else:
+        if not get_tolerance(user_dim, user_it):
             print("\nUnable to calculate, enter values again\n")
+
+
+parser = argparse.ArgumentParser(
+    description="Tolerance value calculator, run without commands for iUI")
+parser.add_argument("dimension", type=float, action="store",
+                    help="Tolerated dimension value (in mm)")
+parser.add_argument("IT_class", action="store", type=int,
+                    help="Tolerance class according to IT")
+# TODO: add unit handling
+# parser.add_argument("--unit", action="store", type=str,
+#                     help="unit in which tolerance value will be returned")
+
+
+def main():
+    """ handler function """
+    if len(sys.argv) == 1:
+        run_ui()
+    else:
+        args = parser.parse_args()
+
+        get_tolerance_str(args.dimension, args.IT_class)
 
 
 if __name__ == "__main__":
